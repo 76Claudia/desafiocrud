16520d3be51646f2268d89220c1ba81b865b2dbe
76Claudia <claudinha.gcruz@gmail.com>
2024-03-24 23:18:42 -0300

    Pageable

--- src/main/java/com/claudiagomes/crud/controllers/ClientController.java
+++ src/main/java/com/claudiagomes/crud/controllers/ClientController.java
@@ -5,8 +5,11 @@ import com.claudiagomes.crud.entities.Client;
 import com.claudiagomes.crud.repositories.ClientRepository;
 import com.claudiagomes.crud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,5 +21,12 @@ public class ClientController {
     public ClientDTO findById(@PathVariable Long id){
        ClientDTO dto = service.findById(id);
         return dto;
+}
+
+@GetMapping
+public Page<ClientDTO> findAll(Pageable pageable){
+
+  return service.findAll(pageable);
+
 }
 }
--- src/main/java/com/claudiagomes/crud/services/ClientService.java
+++ src/main/java/com/claudiagomes/crud/services/ClientService.java
@@ -4,9 +4,12 @@ import com.claudiagomes.crud.dto.ClientDTO;
 import com.claudiagomes.crud.entities.Client;
 import com.claudiagomes.crud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +23,13 @@ public class ClientService {
         ClientDTO dto = new ClientDTO(client);
         return dto;
     }
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+       Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
 
-}
+    }
+
+
+
