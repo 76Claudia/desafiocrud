afdf5ea31dbb3b21c5e5cee57cdb9ffeb75516f9
76Claudia <claudinha.gcruz@gmail.com>
2024-03-26 21:23:12 -0300

    Delete Client

--- src/main/java/com/claudiagomes/crud/controllers/ClientController.java
+++ src/main/java/com/claudiagomes/crud/controllers/ClientController.java
@@ -20,27 +20,36 @@ import java.util.Optional;
 public class ClientController {
     @Autowired
     private ClientService service;
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
-}
+    }
 
-@GetMapping
-public ResponseEntity<Page<ClientDTO>>findAll(Pageable pageable){
-    Page<ClientDTO> dto = service.findAll(pageable);
-    return ResponseEntity.ok(dto);
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
 
-}
+    }
+
     @PostMapping
-    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
-    @PutMapping (value = "/{id}")
-    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto ){
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
